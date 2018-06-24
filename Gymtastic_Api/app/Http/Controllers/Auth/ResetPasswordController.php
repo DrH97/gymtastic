6d8caf2a8dd3e4a9925bfa7ac850a5f555ddfb4b
DrH97 <jmnabangi@gmail.com>
2018-06-24 14:21:31 +0300

    Deep linking App Finalization

--- Gymtastic_Api/app/Http/Controllers/Auth/ResetPasswordController.php
+++ Gymtastic_Api/app/Http/Controllers/Auth/ResetPasswordController.php
@@ -25,7 +25,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/passwordresetsuccess';
+    protected $redirectTo = '/app/login';
 
     /**
      * Create a new controller instance.
